d65713c7ba876f66c68bd0162b8c03793f60c5a4
Dmitry Melkov <melkov@itibsoft.ru>
2024-08-27 11:25:48 +0700

    - feat: start create simple logic for assets manager

---
+++ snake/include/assets/assets-manager.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+namespace assets
+{
+    const std::string ASSETS_ROOT_PATH = "assets";
+    
+    class AssetsManager
+    {
+    public:
+         sf::Texture* load_texture(const std::string& texture_path);
+
+    private:
+        std::map<std::string, sf::Texture*> textures_;
+    };
+}
+
+
---
+++ snake/source/assets/assets-manager.cpp
@@ -0,0 +1,29 @@
+﻿#include "../../include/assets/assets-manager.h"
+
+#include "../../include/utils/logger.h"
+
+namespace assets
+{
+    sf::Texture* AssetsManager::load_texture(const std::string& texture_path)
+    {
+        if(this->textures_.count(texture_path))
+        {
+            return this->textures_[texture_path];
+        }
+        
+        const auto texture = new sf::Texture();
+
+        const std::string target_path = ASSETS_ROOT_PATH + '/' + texture_path;
+
+        if(texture->loadFromFile(target_path, sf::IntRect(10, 10, 32, 32)) == false)
+        {
+            utils::error("Not found texture: %a", target_path);
+
+            return nullptr;
+        }
+
+        this->textures_[texture_path] = texture;
+
+        return texture;
+    }
+}
