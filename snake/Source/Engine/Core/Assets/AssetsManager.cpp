f6aed34f5bccf07a7e69f2de2aa3a8c7ad780440
Dmitry Melkov <melkov@itibsoft.ru>
2024-09-01 16:49:00 +0700

    - feat: start create assets manager

---
+++ snake/Source/Engine/Core/Assets/AssetsManager.cpp
@@ -0,0 +1,23 @@
+﻿#include "AssetsManager.h"
+
+std::map<std::string, sf::Texture*> AssetsManager::_textures;
+
+sf::Texture* AssetsManager::LoadTexture(const std::string& path, const sf::IntRect& area)
+{
+    if(_textures.count(path) != false)
+    {
+        return _textures[path];
+    }
+    
+    const auto texture = new sf::Texture();
+
+    if(texture->loadFromFile(path, area) == false)
+    {
+        std::cout << "Texture not loaded for target path: " << path << "\n";
+        return nullptr;
+    }
+
+    _textures[path] = texture;
+
+    return texture;
+}
---
+++ snake/Source/Engine/Core/Assets/AssetsManager.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <map>
+
+#include "SFML/Graphics/Texture.hpp"
+#include <iostream>
+
+class AssetsManager
+{
+public:
+    static sf::Texture* LoadTexture(const std::string& path, const sf::IntRect& area = sf::IntRect());
+
+private:
+    static std::map<std::string, sf::Texture*> _textures;
+};
