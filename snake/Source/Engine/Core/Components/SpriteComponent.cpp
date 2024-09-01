6e50e973b808b581c055aa500c7326dd4efad88b
Dmitry Melkov <melkov@itibsoft.ru>
2024-09-01 16:49:24 +0700

    - feat: create simple logic for sprite component

---
+++ snake/Source/Engine/Core/Components/SpriteComponent.cpp
@@ -0,0 +1,46 @@
+﻿#include "SpriteComponent.h"
+
+#include "../GameObject.h"
+
+
+SpriteComponent::SpriteComponent(GameObject& gameObject): Component(gameObject)
+{
+}
+
+SpriteComponent::~SpriteComponent()
+{
+}
+
+void SpriteComponent::SetTexture(const sf::Texture& texture)
+{
+    this->_texture = texture;
+
+    this->_sprite.setTexture(texture);
+}
+
+void SpriteComponent::SetSize(const sf::Vector2f& size)
+{
+}
+
+void SpriteComponent::SetColor(const sf::Color& color)
+{
+    this->_sprite.setColor(color);
+}
+
+void SpriteComponent::Update(float deltaTime)
+{
+    Component::Update(deltaTime);
+    
+    this->_sprite.setPosition(this->_gameObject->getPosition());
+    this->_sprite.setRotation(this->_gameObject->getRotation());
+    this->_sprite.setScale(this->_gameObject->getScale());
+}
+
+void SpriteComponent::Render(sf::RenderTarget& renderTarget)
+{
+    renderTarget.draw(this->_sprite);
+}
+
+void SpriteComponent::Dispose()
+{
+}
---
+++ snake/Source/Engine/Core/Components/SpriteComponent.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Component.h"
+
+class SpriteComponent final : public Component
+{
+public:
+    explicit SpriteComponent(GameObject& gameObject);
+    ~SpriteComponent() override;
+
+    void SetTexture(const sf::Texture& texture);
+    void SetSize(const sf::Vector2f& size);
+    void SetColor(const sf::Color& color);
+
+    void Update(float deltaTime) override;
+    void Render(sf::RenderTarget& renderTarget) override;
+    void Dispose() override;
+
+private:
+    sf::Sprite _sprite;
+    sf::Texture _texture;
+};
