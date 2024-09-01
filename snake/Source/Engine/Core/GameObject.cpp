0326764fa2c0d05284cf590bd1d524413339bb21
Dmitry Melkov <melkov@itibsoft.ru>
2024-09-01 16:48:30 +0700

    - fix: crash application witch fixed unique_ptr

--- snake/Source/Engine/Core/GameObject.cpp
+++ snake/Source/Engine/Core/GameObject.cpp
@@ -1,9 +1,33 @@
 ﻿#include "GameObject.h"
 
+#include <iostream>
 GameObject::~GameObject()
 {
 }
 void GameObject::AddComponent(Component& component)
+template <typename TComponent>
+TComponent* GameObject::AddComponent()
+{
+    const auto isComponent = std::is_base_of_v<Component, TComponent>;
+    static_assert(isComponent, "TComponent must be a subclass of Component");
+
+    auto component = std::make_unique<TComponent>(*this);
+    
+    if (!component)
+    {
+        std::cerr << "Failed to create component\n";
+        return nullptr;
+    }
+
+    TComponent* componentPtr = component.get();
+    component.release();
+
+    const auto result = std::move(componentPtr);
+    
+    _components.push_back(result);
+
+    return result;
+}
 {
     this->_components.push_back(&component);
 }
