2f2fc78c361eba3421434d002e48d656d9af2e3a
Dmitry Melkov <melkov@itibsoft.ru>
2024-09-05 02:57:28 +0700

    - feat: create simple logic for fps counter

---
+++ snake/Source/Game/Debug/FPSCounter.cpp
@@ -0,0 +1,17 @@
+﻿#include "FPSCounter.h"
+
+#include "imgui.h"
+
+void FPSCounter::DrawFpsText(const float deltaTime)
+{
+    _elapsedTime += deltaTime;
+    _frameCount++;
+
+    if (_elapsedTime >= 1.0f) {
+        _fps = static_cast<float>(_frameCount) / _elapsedTime;
+        _elapsedTime = 0.0f;
+        _frameCount = 0;
+    }
+    
+    ImGui::Text("FPS: %f", _fps);
+}
---
+++ snake/Source/Game/Debug/FPSCounter.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+class FPSCounter
+{
+public:
+    void DrawFpsText(float deltaTime);
+
+private:
+    float _elapsedTime{0.0f};
+    int _frameCount{0};
+    float _fps{0.0f};
+};
