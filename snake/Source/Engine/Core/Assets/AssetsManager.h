53114e7c59e799499e1edc6ba845301d7f78155c
Dmitry Melkov <melkov@itibsoft.ru>
2024-09-08 11:38:06 +0700

    - update assets manager

--- snake/Source/Engine/Core/Assets/AssetsManager.h
+++ snake/Source/Engine/Core/Assets/AssetsManager.h
@@ -1,14 +1,34 @@
 ﻿#pragma once
+#include <iostream>
 #include <map>
 
 #include "SFML/Graphics/Texture.hpp"
-#include <iostream>
 
 class AssetsManager
 {
 public:
-    static sf::Texture* LoadTexture(const std::string& path, const sf::IntRect& area = sf::IntRect());
+    static sf::Texture* LoadTexture(const std::string& path, const sf::IntRect& area = sf::IntRect())
+    {
+        std::string key = path + std::to_string(area.left) + std::to_string(area.top) + std::to_string(area.width) + std::to_string(area.height);
+
+        if (_textures.count(key))
+        {
+            return _textures[key];
+        }
+
+        const auto texture = new sf::Texture();
+
+        if (!texture->loadFromFile(path, area))
+        {
+            std::cout << "Texture not loaded for target path: " << path << "\n";
+            return nullptr;
+        }
+
+        _textures[key] = texture;
+
+        return texture;
+    }
 
 private:
-    static std::map<std::string, sf::Texture*> _textures;
+    inline static std::map<std::string, sf::Texture*> _textures;
 };
